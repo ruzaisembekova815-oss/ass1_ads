17828e0c71d9929feaaf158580347719a2b8aba9
ruzaisembekova815-oss <253458@astanait.edu.kz>
2026-03-26 16:02:23 +0300

    7

--- src/Square.java
+++ src/Square.java
@@ -1,35 +1,35 @@
 import java.util.Scanner;
 
 public class Square {
-    // Рекурсивная функция заполнения спирали (по слоям)
+    
     static void fillSpiral(int[][] mat, int top, int left, int bottom, int right, int num) {
         if (top > bottom || left > right) return;
 
-        // 1. Верхняя строка слева направо
+        
         for (int i = left; i <= right; i++) {
             mat[top][i] = num++;
         }
 
-        // 2. Правая колонка сверху вниз
+       
         for (int i = top + 1; i <= bottom; i++) {
             mat[i][right] = num++;
         }
 
-        // 3. Нижняя строка справа налево (если есть)
+        
         if (top < bottom) {
             for (int i = right - 1; i >= left; i--) {
                 mat[bottom][i] = num++;
             }
         }
 
-        // 4. Левая колонка снизу вверх (если есть)
+       
         if (left < right) {
             for (int i = bottom - 1; i > top; i--) {
                 mat[i][left] = num++;
             }
         }
 
-        // Рекурсия — переходим к внутреннему слою
+        
         fillSpiral(mat, top + 1, left + 1, bottom - 1, right - 1, num);
     }
 
